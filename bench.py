433691c3c8b864c3f2200a92a9f7e3df0f587140
brent iverson <biverson@pacbell.net>
2026-01-21 11:58:26 -0600

    reworked the benchmark script a little

--- bench.py
+++ bench.py
@@ -4,30 +4,29 @@
 import time
 
 #-------------------------------------------------------------
-DAYS = 25
-codes = [None] * DAYS
-times = [0] * DAYS
+MIN_DAY = 1
+MAX_DAY = 25
+codes = [None] * (MAX_DAY + 1)
+times = [0] * (MAX_DAY + 1)
 
 print('================ loading files ================')
-for day in range(DAYS):
-	filename = f'day{day + 1}.py'
+for day in range(MIN_DAY, MAX_DAY + 1):
+	filename = f'day{day}.py'
 	try:
 		with open(filename) as f:
-			print(day + 1)
-			code = compile(f.read(), filename, 'exec')
-			codes[day] = code
+			codes[day] = compile(f.read(), filename, 'exec')
 	except FileNotFoundError:
 		pass
 
-for day in range(DAYS):
+for day in range(MIN_DAY, MAX_DAY + 1):
 	startTime = time.time()
 	if codes[day]:
-		print('================ day', day + 1, '================')
+		print('================ day', day, '================')
 		exec(codes[day], None, None)
 		times[day] = time.time() - startTime
 
 print('================ times ================')
-for day in range(DAYS):
+for day in range(MIN_DAY, MAX_DAY + 1):
 	if codes[day]:
-		print('day', day + 1, '=', round(times[day], 2), 'seconds')
+		print('day', day, '=', round(times[day], 2), 'seconds')
 print('================ total time', round(sum(times), 1), 'seconds ================')
